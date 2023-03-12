200d56c0890f7aed53216f067de4777ad7b7c5fc
SNL23 <steph@henahosa.com>
2023-03-12 16:03:16 -0700

    updated variables and if statments

--- script.js
+++ script.js
@@ -5,64 +5,65 @@
 var generateBtn = document.querySelector("#generate");
 var lowercaseOptions = "abcdefghijklomnpqrstuvwxyz";
 var uppercaseOptions = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var  numberic = "0123456789";
+var numberic = "0123456789";
 var specail = "!@#$%^&*.";
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var password = document.querySelector("#password");
+  var finalpassword = generatePassword();
+  var passwordcontainer = document.querySelector("#password");
 
-  password.value = password;
+  passwordcontainer.textContent = finalpassword;
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 // WHEN I click the button to generate a password
-function generatePassword(){
-  var password = ""
-}
+
 // THEN I am presented with a series of prompts for password criteria
-var prompt =("Do you want a lowercase?");
-var prompt =("Do you want a uppercase?");
-var prompt =("Do you want a number?");
-var prompt =("Do you want a do special character?");
+
 
 // THEN I select which criteria to include in the password
 // WHEN prompted for the length of the password
 // THEN I choose a length of at least 8 characters and no more than 128 characters
-var passwordLength = prompt("How long would you like your password?");
-function length() {
-  var prompt;
+function options() {
+  var passwordLength = prompt("How long would you like your password?");
+  var optlowercase = confirm("Do you want a lowercase?");
+  var optuppercase = confirm("Do you want a uppercase?");
+  var optnumeric = confirm("Do you want a number?");
+  var optspecial = confirm("Do you want a do special character?");
+  var optpwdOptions = { passwordLength, optlowercase, optuppercase, optnumeric, optspecial }
+  return optpwdOptions;
 }
 // WHEN asked for character types to include in the password
 // THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
 
-var lowercase = confirm("Do you want a lowercase?");
-var uppercase = confirm("Do you want an uppercase?");
-var numeric = confirm("Do you want a number?");
-var special = confirm("Do you want a special character?");
-
 // WHEN I answer each prompt
 // THEN my input should be validated and at least one character type should be selected
-
-if(lowercase) {
-  password = password + getRandomValue(lowercaseOptions);
-} else if (uppercase) {
-  password = password + getRandomValue(uppercaseOptions);
-} else if(numeric) {
-  password = password + getRandomValue(numeric);
-} else if (special) {
-  password = password + getRandomValue(special);
+function generatePassword() {
+  var opt = options()
+  console.log(opt);
+  var password = ""
+  if (opt.optlowercase) {
+    password = password + getRandomValue(lowercaseOptions);
+  } if (opt.optuppercase) {
+    password = password + getRandomValue(uppercaseOptions);
+  } if (opt.optnumeric) {
+    password = password + getRandomValue(numberic);
+  } if (opt.optspecial) {
+    password = password + getRandomValue(specail);
+  }
+  // THEN a password is generated that matches the selected criteria
+  for (var i = 0; i < opt.passwordLength; i++) {
+    password = password += getRandomValue(lowercaseOptions);
+  }
+  console.log(password)
+  return password;
 }
-// THEN a password is generated that matches the selected criteria
-for (var i = 0; i < password.length, i++;) {
-  password = password + getRandomValue(lowercaseOptions);
-} 
 
 // THEN the password is either displayed in an alert or written to the page 
 
-function getRandomValue(str){
+function getRandomValue(str) {
   var randomIndex = Math.floor(Math.random() * str.length);
   return str[randomIndex];
 }
