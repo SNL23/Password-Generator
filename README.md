b3ca95a6095ec209e93202ac12508dc08f939082
SNL23 <steph@henahosa.com>
2023-03-13 21:57:35 -0700

    finished the README file

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# Password Generator Starter Code
+# Password Generator
+
+## Description
+
+This webpage was designed to help the user create a password that meets certain ciriteria.
+This webpage was made to generate a strong password that provides greater security.
+In this project I was able to create a serious of prompts for the password criteria along with the password length. Then I created a function for every prompted confirmed so that whatever criteria the comfirmed is the one that would show up once the password was generated.
+with this projected I learned how valueable functions are when preforming specific task. I also learned how important it is to have things spelled right for your code to work.
+
+## License
+
+please refer to the license in the repo.
--- script.js
+++ script.js
@@ -1,31 +1,20 @@
 // Assignment code here
 
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 var lowercaseOptions = "abcdefghijklomnpqrstuvwxyz";
 var uppercaseOptions = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numberic = "0123456789";
 var specail = "!@#$%^&*.";
 
-// Write password to the #password input
 function writePassword() {
   var finalpassword = generatePassword();
   var passwordcontainer = document.querySelector("#password");
 
   passwordcontainer.textContent = finalpassword;
 }
-// Add event listener to generate button
+
 generateBtn.addEventListener("click", writePassword);
 
-// WHEN I click the button to generate a password
-
-// THEN I am presented with a series of prompts for password criteria
-
-
-// THEN I select which criteria to include in the password
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
 function options() {
   var passwordLength = prompt("How long would you like your password?");
   var optlowercase = confirm("Do you want a lowercase?");
@@ -35,11 +24,7 @@ function options() {
   var optpwdOptions = { passwordLength, optlowercase, optuppercase, optnumeric, optspecial }
   return optpwdOptions;
 }
-// WHEN asked for character types to include in the password
-// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
 
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
 function generatePassword() {
   var opt = options()
   console.log(opt);
@@ -53,15 +38,21 @@ function generatePassword() {
   } if (opt.optspecial) {
     password = password + getRandomValue(specail);
   }
-  // THEN a password is generated that matches the selected criteria
+  
   for (var i = 0; i < opt.passwordLength; i++) {
-    password = password += getRandomValue(lowercaseOptions);
+    if (opt.optlowercase) {
+      password = password + getRandomValue(lowercaseOptions);
+    } if (opt.optuppercase) {
+      password = password + getRandomValue(uppercaseOptions);
+    } if (opt.optnumeric) {
+      password = password + getRandomValue(numberic);
+    } if (opt.optspecial) {
+      password = password + getRandomValue(specail);
+    }
   }
   console.log(password)
-  return password;
+  return password.substring(0, opt.passwordLength);
 }
-
-// THEN the password is either displayed in an alert or written to the page 
 
 function getRandomValue(str) {
   var randomIndex = Math.floor(Math.random() * str.length);
